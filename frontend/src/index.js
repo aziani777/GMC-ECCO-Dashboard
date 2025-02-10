0005130bb1c43ffb1e23ec32bffd849579122915
aziani777 <adzian.mcc@gmail.com>
2025-02-10 10:55:32 +0100

    Add Material-UI theme and proper styling setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';  // We'll create this
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 ); 
---
+++ frontend/src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#6200ea', // Purple color
+    },
+    secondary: {
+      main: '#3f51b5',
+    },
+  },
+});
+
+export default theme; 
